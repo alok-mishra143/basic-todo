02bc1097d6a42bd64a22051f24a18897026c27dc
alok_mishra14 <143alokmishra143@gmail.com>
2024-12-01 10:39:49 +0530

    adding the mongodb

--- backend/db.js
+++ backend/db.js
@@ -2,9 +2,18 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const Todo = new Schema({
+const url = process.env.MONGO - URI || " ";
+
+mongoose.connect(url);
+
+const TodoSchema = mongoose.Schema({
   title: String,
   description: String,
+  completed: Boolean,
 });
 
+const Todo = mongoose.model("todos", TodoSchema);
 
+module.exports = {
+  Todo,
+};
